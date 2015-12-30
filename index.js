b59927d2dce3555c969e85d60c1608e33156c1c1
Andy Brett <andy@andybrett.com>
2015-12-30 10:50:49 -0800

    basic index

---
+++ index.js
@@ -0,0 +1,24 @@
+require('dotenv').load();
+var slack = require("slack");
+var express = require('express');
+var app = express();
+var url = require('url');
+var request = require('request');
+
+app.set('port', (process.env.PORT || 5000));
+
+app.get('/auth', function(req, res) {
+  slack.oauth.access({
+    "client_id": process.env.CLIENT_ID,
+    "client_secret": process.env.CLIENT_SECRET,
+    "code": req.params["code"]
+  }, function(request, response) {
+    res.send("You've authorized Beeminder! Probably.");
+    // parse the response and save to redis
+  })
+});
+
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
