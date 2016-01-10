07e5eeea6558f780be1a15db073a6505dcf25dd7
Andy Brett <andy@andybrett.com>
2016-01-10 09:05:24 -0800

    debug

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({ extended: true }));
 var url = require('url');
 var request = require('request');
 var slackClient = require('slack-client');
