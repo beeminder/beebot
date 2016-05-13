ffaeb65a2123aa45b73f43f044b742e33b5cfc37
Andy Brett <andy@andybrett.com>
2016-05-13 18:55:00 +1200

    delete bids too

--- index.js
+++ index.js
@@ -196,7 +196,9 @@ app.post('/bid', function(req, res) {
       } else if (text.match(/abort/i)) {
         var purpose = obj.purpose;
         redis.del("beebot.auctions." + req.body.channel_id, function(err, obj) {
-          res.send("Okay, aborted the bidding for " + purpose);
+          redis.del("beebot.auctions." + req.body.channel_id + ".bids", function(err, obj) {
+            res.send("Okay, aborted the bidding for " + purpose);
+          });
         });
       } else if (text.match(/@/)) {
         res.send("You can't submit a bid with an @-mention. There is currently an active auction for " + obj.purpose + ". Use `/bid abort` to end the active auction or `/bid` to check status.")
