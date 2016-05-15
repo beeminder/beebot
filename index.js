be1b8d2cffa3c7b715cd82684d3ab8faa7e60f69
Daniel Reeves <dreeves@beeminder.com>
2016-05-15 03:10:23 -0700

    Undo purpose->urtext #strawgrasp #dosequisman

--- index.js
+++ index.js
@@ -234,7 +234,7 @@ app.post('/bid', function(req, res) {
         bidStatusShout(res, chan)
       } else if(text.match(/help/i)) {
         shout(res, "Currently active auction initiated by @" + "TODO:initiator"
-          + " via:\n`" + obj.urtext + "`\n" + bidHelp)
+          + " via:\n`" + obj.purpose + "`\n" + bidHelp)
       } else if(text.match(/abort/i)) {
         bidEnd(chan)
         shout(res, "Aborted.") // TODO: want latest bid status here too
@@ -276,7 +276,7 @@ app.post('/bid', function(req, res) {
         redis.hmset("beebot.auctions." + chan + ".bids", bids, 
                     function(err, obj) { })
         var auction = {}
-        auction.urtext = "/bid " + text.trim()
+        auction.purpose = "/bid " + text.trim()
         //auction.initiator = user
         redis.hmset("beebot.auctions." + chan, auction, function(err, obj) {
           bidStatusShout(res, chan)
