8965a35368464eb05579014f470aa0cd9f771157
Andy Brett <andy@andybrett.com>
2016-05-26 07:58:24 -0700

    rename botils, consistent style

--- index.js
+++ index.js
@@ -14,10 +14,10 @@ var https      = require('https');
 var http       = require('http');
 var bodyParser = require('body-parser');
 
-var beetils    = require('./lib/beetils.js');
 var beebot     = require('./lib/beebot.js');
 var bid        = require('./lib/bid.js');
 var tock       = require('./lib/tock.js');
+var roll       = require('./lib/roll.js');
 
 var app = express();
 app.use(bodyParser.json());
@@ -50,33 +50,18 @@ app.post('/tock', function(req, res) {
 })
 
 app.post("/tockcheck", function(req, res) {
-  // end all tocks with end dates < now, post as failures to channel
   tock.handleTockcheck(req, res);
 })
 
-// START ROLL COMMAND //
 app.post('/roll', function(req, res) {
-  var text = req.body.text
-  var n = parseInt(text)
-  if(isNaN(n)) {
-    beetils.whisp(res, "Pssst, this is not an integer: " + text)
-  } else if(n <= 0) {
-    beetils.shout(res, "Rolling " + n + "-sided die... "
-      + (beetils.bern(0.1) ? ":poop:" : ":boom:")
-      + " (try again with a positive number of sides?)")
-  } else {
-    beetils.shout(res, "Rolling " + n + "-sided die... it came up " + beetils.randint(n))
-  }
-  // TODO: whisper help in response to /roll or /roll help
+  roll.handleSlash(req, res);
 })
-// END ROLL COMMAND //
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
   redis.keys("beebot.teamid.*", function(err, obj) {
     for (var i = 0; i < obj.length; i++) {
       var teamId = obj[i].split(".").pop();
-      console.log(teamId);
       beebot.startBot(teamId);
     }
   });
