a9af77526b609291de9ab92ab2d25e1f23dff93e
Andy Brett <andy@andybrett.com>
2016-06-02 09:00:33 -0400

    log request body

--- index.js
+++ index.js
@@ -25,40 +25,48 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set('port', (process.env.PORT || 5000));
 
 app.post('/bot', function(req, res) {
+  console.log("Request body: " + JSON.stringify(req.body));
   beebot.handleCreateBot(req, res);
 });
 
 app.delete('/bot', function(req, res) {
+  console.log("Request body: " + JSON.stringify(req.body));
   // TODO: delete a bot, if a user deauths from a team.
   // basically just need to delete the teamId from redis so we don't keep trying
   // to create it on restarts
 });
 
 app.post('/zeno', function(req, res) {
+  console.log("Request body: " + JSON.stringify(req.body));
   beebot.handleZeno(req, res);
 });
 
 app.get('/debugger', function(req, res) { debugger; });
 
 app.post('/bid', (req, res) => {
+  console.log("Request body: " + JSON.stringify(req.body));
   bid.handleSlash(req, res);
 })
 
 app.post('/tock', function(req, res) {
+  console.log("Request body: " + JSON.stringify(req.body));
   tock.handleSlash(req, res);
 })
 
 app.post("/check", function(req, res) {
+  console.log("Request body: " + JSON.stringify(req.body));
   tock.checkTocks();
   charge.checkCharges();
   res.send("ok");
 })
 
 app.post('/roll', function(req, res) {
+  console.log("Request body: " + JSON.stringify(req.body));
   roll.handleSlash(req, res);
 })
 
 app.post('/charge', function(req, res) {
+  console.log("Request body: " + JSON.stringify(req.body));
   charge.handleSlash(req, res);
 });
 
